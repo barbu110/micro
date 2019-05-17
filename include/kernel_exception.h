6fd603d47e7a9c114159c5c1af5808bc74bd4cbc
Victor Barbu <victorbarbu08@gmail.com>
2019-05-17 23:43:33 +0300

    Add microloop::KernelException

---
+++ include/kernel_exception.h
@@ -0,0 +1,16 @@
+// Copyright 2019 Victor Barbu.
+
+#include <stdexcept>
+
+#include <errno.h>
+#include <cstring>
+
+namespace microloop {
+
+// Wrapper around error codes returned by system calls.
+class KernelException : public std::runtime_error {
+ public:
+  explicit KernelException(int err) : std::runtime_error{strerror(err)} {}
+};
+
+}  // namespace microloop
