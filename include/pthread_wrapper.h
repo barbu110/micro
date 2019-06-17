16d8d0483b77b0ce306dd405c02ace9fd3443c64
Stefan Silviu <sylviu44@gmail.com>
2019-06-17 22:41:17 +0300

    Document PThread class

--- include/pthread_wrapper.h
+++ include/pthread_wrapper.h
@@ -27,7 +27,7 @@ enum AttrDetachState : int {
 
 enum AttrThreadScope : int {
   SYSTEM = PTHREAD_SCOPE_SYSTEM,
-  PROCESS = PTHREAD_SCOPE_PROCESS
+  PROCESS = PTHREAD_SCOPE_PROCESS /// Unsupported operation on Linux
 };
 
 enum AttrInheritScheduler : int {
@@ -38,7 +38,7 @@ enum AttrInheritScheduler : int {
 enum AttrSchedulingPolicy : int {
   FIFO = SCHED_FIFO,
   ROUND_ROBIN = SCHED_RR,
-  OTHER = SCHED_OTHER // Default Linux time-sharing scheduling
+  OTHER = SCHED_OTHER /// Default Linux time-sharing scheduling
 };
 
 struct StackInfo {
@@ -46,6 +46,10 @@ struct StackInfo {
   std::size_t size;
 };
 
+/**
+ * Builder wrapper for @ref pthread_attr_t.
+ * @sa PThread
+ */
 class PThreadAttributes {
   pthread_attr_t attr;
 public:
@@ -83,6 +87,9 @@ public:
 
 using ThreadRoutine = void*(void*);
 
+/**
+ * High-level representation of a thread handle. Implements various common thread operations.
+ */
 class PThread {
   pthread_t handle;
   bool detached = false;
@@ -90,8 +97,21 @@ class PThread {
   /** Used for making shallow copies in @ref self. */
   explicit PThread(pthread_t) noexcept;
 
+  /**
+   * Internal constructor for calling @ref pthread_create.
+   * @param targetRoutine function to run on new thread
+   * @param routineData data to pass to targetRoutine
+   * @param attrs attributes for the new thread
+   */
   PThread(ThreadRoutine targetRoutine, void* routineData, const pthread_attr_t* attrs);
 
+  /**
+   * Internal constructor for isolating usage of reinterpret_cast.
+   * @tparam Functor highLevelRoutine's type
+   * @param carrierRoutine a Functor-specific wrapper for bootstrapping the highLevelRoutine
+   * @param highLevelRoutine the actual code to run on the new thread
+   * @param attrs attributes for the new thread
+   */
   template<
       typename Functor,
       typename std::enable_if_t<
@@ -100,18 +120,30 @@ class PThread {
       PThread(carrierRoutine, reinterpret_cast<void*>(highLevelRoutine), attrs) {}
 
 public:
+  /** Create a thread and run the specified routine. */
   explicit PThread(ThreadRoutine);
+  /** Create a thread with the given attributes and run the specified routine. */
   PThread(ThreadRoutine, const PThreadAttributes&);
 
+  /** Create a thread and run the specified routine. */
   explicit PThread(std::function<void()>);
+  /** Create a thread with the given attributes and run the specified routine. */
   PThread(std::function<void()>, const PThreadAttributes&);
 
+  /** Create a thread and run the specified routine that returns a value. */
   static PThread makeWithRet(std::function<void*()>);
+  /** Create a thread with the given attributes and run the specified routine that returns a value. */
   static PThread makeWithRet(std::function<void*()>, const PThreadAttributes&);
 
+  /** Returns the @ref PThread object for the current thread. */
   static PThread self() noexcept;
+  /** Wrapper for @ref pthread_exit. */
   static noreturn void exit(void* retVal = nullptr) noexcept;
 
+  /**
+   * Safer wrapper for @ref pthread_join.
+   * @tparam T cast the thread's return value to a pointer of this type
+   */
   template<typename T = void>
   T* join() {
     void* retValPtr;
@@ -120,6 +152,7 @@ public:
     return static_cast<T*>(retValPtr);
   }
 
+  /** Detaches this thread after construction. */
   void detach();
 
   bool operator==(const PThread& other) const noexcept;
