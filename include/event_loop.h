2599b36bead6068b51ed601d9ec1b9870dd7a257
Victor Barbu <victorbarbu08@gmail.com>
2019-07-27 16:17:55 +0300

    Move function definition for EventLoop to the corresponding source file

--- include/event_loop.h
+++ include/event_loop.h
@@ -5,129 +5,66 @@
 #pragma once
 
 #include <cstdint>
-#include <errno.h>
-#include <event_source.h>
-#include <kernel_exception.h>
-#include <limits>
 #include <map>
-#include <signal.h>
 #include <signals_monitor.h>
-#include <stdexcept>
-#include <sys/epoll.h>
-#include <sys/signalfd.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <utils/thread_pool.h>
-#include <vector>
+
+#include "event_source.h"
+#include "utils/thread_pool.h"
 
 namespace microloop
 {
 
 class EventLoop
 {
-  EventLoop() : thread_pool{4}
-  {
-    epollfd = epoll_create(1);
-    if (epollfd == -1)
-    {
-      throw KernelException(errno);
-    }
-
-    add_event_source(&signals_monitor);
-  }
+  /**
+   * Create an instance of the event loop. The thread pool is initialized with 4 threads by default.
+   */
+  EventLoop();
 
 public:
-  static EventLoop *get_main()
-  {
-    if (main_instance == nullptr)
-    {
-      main_instance = new EventLoop;
-    }
+  /**
+   * @return An instance of the event loop.
+   */
+  static EventLoop *get_main();
 
-    return main_instance;
-  }
-
+  /**
+   * @return The embedded thread pool.
+   */
   utils::ThreadPool &get_thread_pool()
   {
     return thread_pool;
   }
 
-  void add_event_source(EventSource *event_source)
-  {
-    std::uint32_t fd = event_source->get_fd();
+  /**
+   * Add a new event source to the event loop.
+   * @param event_source The event source to be added.
+   */
+  void add_event_source(EventSource *event_source);
 
-    auto produced_events = event_source->produced_events();
-    if (!produced_events)
-    {
-      throw std::invalid_argument("produced_events is 0");
-    }
+  /**
+   * Remove an existing event source from the event loop.
+   * @param event_source The event source to remove.
+   */
+  void remove_event_source(EventSource *event_source);
 
-    epoll_event ev{};
-    ev.events = produced_events;
-    ev.data.ptr = static_cast<void *>(event_source);
-
-    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &ev) == -1)
-    {
-      throw microloop::KernelException(errno);
-    }
-
-    event_sources[fd]
-        = event_source;  // TODO Do we still need storing event soucres in the hashtable?
-
-    if (event_source->native_async())
-    {
-      event_source->start();
-    }
-    else
-    {
-      thread_pool.submit(&EventSource::start, event_source);
-    }
-  }
-
-  void remove_event_source(EventSource *event_source)
-  {
-    if (epoll_ctl(epollfd, EPOLL_CTL_DEL, event_source->get_fd(), nullptr) == -1)
-    {
-      throw KernelException(errno);
-    }
-
-    event_sources.erase(event_source->get_fd());
-    delete event_source;
-  }
-
+  /**
+   * Register a new signal handler.
+   * @param sig The signal that the handler responds to. This signal will be blocked via the
+   * process sigmask.
+   * @param callback The callback to be called when signal `sig` is caught.
+   */
   inline void register_signal_handler(std::uint32_t sig, SignalsMonitor::SignalHandler &&callback)
   {
     signals_monitor.register_signal_handler(sig, std::move(callback));
   }
 
-  bool next_tick()
-  {
-    epoll_event events_list[32];
-
-    auto ready = epoll_pwait(epollfd, events_list, 32, -1, &signals_monitor.get_sigmask());
-    if (ready < 0)
-    {
-      return false;
-    }
-
-    for (int i = 0; i < ready; i++)
-    {
-      auto &event = events_list[i];
-
-      auto event_source = reinterpret_cast<EventSource *>(event.data.ptr);
-
-      if (event_source->needs_retry())
-      {
-        thread_pool.submit(&EventSource::start, event_source);
-      }
-      else
-      {
-        event_source->run_callback();
-      }
-    }
-
-    return true;
-  }
+  /**
+   * Run the next tick of the event loop. This is responsible for waiting for the registered
+   * event sources to trigger, signals to be caught and callbacks to be called for each of the
+   * aforementioned events.
+   * @return Returns `true` if the event loop should continue its execution, `false` otherwise.
+   */
+  bool next_tick();
 
   ~EventLoop()
   {
@@ -135,7 +72,7 @@ public:
   }
 
 private:
-  int epollfd;
+  std::uint32_t epollfd;
   utils::ThreadPool thread_pool;
   SignalsMonitor signals_monitor;
   std::map<std::uint64_t, EventSource *> event_sources;
--- src/event_loop.cpp
+++ src/event_loop.cpp
@@ -1,10 +1,109 @@
 // Copyright 2019 Victor Barbu
 
-#include <event_loop.h>
+#include "event_loop.h"
+
+#include <errno.h>
+#include <kernel_exception.h>
+#include <sys/epoll.h>
 
 namespace microloop
 {
 
 EventLoop *EventLoop::main_instance = nullptr;
+
+EventLoop::EventLoop() : thread_pool{4}
+{
+  epollfd = epoll_create(1);
+  if (epollfd == -1)
+  {
+    throw KernelException(errno);
+  }
+
+  add_event_source(&signals_monitor);
+}
+
+EventLoop *EventLoop::get_main()
+{
+  if (main_instance == nullptr)
+  {
+    main_instance = new EventLoop;
+  }
+
+  return main_instance;
+}
+
+void EventLoop::add_event_source(EventSource *event_source)
+{
+  std::uint32_t fd = event_source->get_fd();
+
+  auto produced_events = event_source->produced_events();
+  if (!produced_events)
+  {
+    throw std::invalid_argument("produced_events is 0");
+  }
+
+  epoll_event ev{};
+  ev.events = produced_events;
+  ev.data.ptr = static_cast<void *>(event_source);
+
+  if (epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &ev) == -1)
+  {
+    throw KernelException(errno);
+  }
+
+  event_sources[fd]
+      = event_source;  // TODO Do we still need storing event soucres in the hashtable?
+
+  if (event_source->native_async())
+  {
+    event_source->start();
+  }
+  else
+  {
+    thread_pool.submit(&EventSource::start, event_source);
+  }
+}
+
+void EventLoop::remove_event_source(EventSource *event_source)
+{
+  if (epoll_ctl(epollfd, EPOLL_CTL_DEL, event_source->get_fd(), nullptr) == -1)
+  {
+    throw KernelException(errno);
+  }
+
+  event_sources.erase(event_source->get_fd());
+  delete event_source;
+}
+
+bool EventLoop::next_tick()
+{
+  epoll_event events_list[32];
+
+  auto ready = epoll_pwait(epollfd, events_list, 32, -1, &signals_monitor.get_sigmask());
+  if (ready < 0)
+  {
+    return false;
+  }
+
+  for (int i = 0; i < ready; i++)
+  {
+    auto &event = events_list[i];
+
+    auto event_source = reinterpret_cast<EventSource *>(event.data.ptr);
+
+    if (event_source->needs_retry())
+    {
+      thread_pool.submit(&EventSource::start, event_source);
+    }
+    else
+    {
+      event_source->run_callback();
+    }
+  }
+
+  return true;
+}
+
+
 
 }  // namespace microloop
