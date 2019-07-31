63f779753e131588525ccf177d66bc2920ac355b
Victor Barbu <victorbarbu08@gmail.com>
2019-07-31 22:38:20 +0300

    Fix the Timeout event source
    Closes #12

--- include/event_sources/timeout.h
+++ include/event_sources/timeout.h
@@ -12,25 +12,13 @@
 namespace microloop::event_sources
 {
 
+template <class Callback>
 class Timeout : public microloop::EventSource
 {
-  using Types = TypeHelper<>;
-
 public:
-  Timeout(int timeout, Types::Callback callback) :
+  Timeout(int timeout, Callback callback) :
       microloop::EventSource{}, timeout{timeout}, callback{callback}
-  {}
-
-  virtual ~Timeout() override
   {
-    int fd = get_fd();
-    close(fd);
-  }
-
-protected:
-  virtual void start() override
-  {
-    errno = 0;
     int fd = timerfd_create(CLOCK_REALTIME, TFD_NONBLOCK);
     if (fd == -1)
     {
@@ -46,9 +34,43 @@ protected:
     }
   }
 
+  virtual ~Timeout() override
+  {
+    close(get_fd());
+  }
+
+protected:
+  virtual void start() override
+  {}
+
   virtual void run_callback() override
   {
+    std::uint64_t expirations_count = 0;
+    ssize_t nread = read(get_fd(), &expirations_count, sizeof(std::uint64_t));
+    if (nread == -1)
+    {
+      /*
+       * According to the man-page, EAGAIN/EWOULDBLOCK could be returned if no timer expirations
+       * have occurred yet. If the application somehow gets in that state, it is certainly an error
+       * state, because at the pooint "run_callback" is executed, the timer file descriptor should
+       * be readable.
+       *
+       * See http://man7.org/linux/man-pages/man2/timerfd_create.2.html
+       */
+      throw microloop::KernelException(errno);
+    }
+
+    /*
+     * We only read the expirations count to consume the event on the timer as this value is of no
+     * importance.
+     */
+
     callback();
+  }
+
+  virtual std::uint32_t produced_events() const override
+  {
+    return EPOLLIN;
   }
 
 private:
@@ -56,11 +78,7 @@ private:
   int timeout;
 
   // The callable object to call when the timer finishes.
-  Types::Callback callback;
+  Callback callback;
 };
 
 }  // namespace microloop::event_sources
-
-#define MICROLOOP_SET_TIMEOUT(on_done, ms)                                                         \
-  microloop::EventLoop::get_main()->add_event_source(                                              \
-      new microloop::event_sources::Timeout(on_done, ms));
