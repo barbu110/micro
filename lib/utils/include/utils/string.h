1dab7c5c314143279f3490de03e21ad47dd7dce2
Victor Barbu <victorbarbu08@gmail.com>
2019-10-15 22:59:34 +0300

    [WIP] Implement utilities library
    Implemented "micro::utils::ends_with" for any string-like type (i.e.
    std::string, std::string_view, char *, const char *).
    
    The method also accepts different cv-qualifiers for the two parameters.

---
+++ lib/utils/include/utils/string.h
@@ -0,0 +1,49 @@
+//
+// Copyright (c) 2019 by Victor Barbu. All Rights Reserved.
+//
+
+#pragma once
+
+#include <type_traits>
+
+namespace utils::string
+{
+
+template <typename T>
+inline constexpr bool is_c_string = std::is_same_v<T, char *> || std::is_same_v<T, const char *>;
+
+template <typename T, typename U>
+std::enable_if_t<!is_c_string<T> && !is_c_string<U>, bool>
+ends_with(T sv, U needle) noexcept
+{
+  if (needle.empty())
+  {
+    return true;
+  }
+
+  if (sv.size() < needle.size())
+  {
+    return false;
+  }
+
+  if (sv == needle)
+  {
+    return true;
+  }
+
+  typename T::size_type p = sv.rfind(needle);
+  if (p == T::npos)
+  {
+    return false;
+  }
+
+  return sv.size() - p == needle.size();
+}
+
+template <typename T, typename U>
+std::enable_if_t<is_c_string<T> && is_c_string<U>, bool>
+ends_with(T sv, U needle) noexcept {
+  return ends_with(std::string_view(sv), std::string_view(needle));
+}
+
+}  // namespace utils::string
---
+++ lib/utils/tests/string_test.cpp
@@ -0,0 +1,48 @@
+//
+// Copyright (c) 2019 by Victor Barbu. All Rights Reserved.
+//
+
+#include "gtest/gtest.h"
+
+#include "utils/string.h"
+
+#include <string>
+#include <tuple>
+
+namespace utils::string
+{
+
+class EndsWithTest : public testing::TestWithParam<std::tuple<std::string, std::string, bool>>
+{};
+
+TEST_P(EndsWithTest, Works)
+{
+  auto [sv, needle, expected] = GetParam();
+  auto actual = ends_with(sv, needle);
+
+  EXPECT_EQ(actual, expected);
+}
+
+TEST(EndsWithTest, WorksWithConstCString)
+{
+  const char *str = "foo";
+  const char *needle = "o";
+
+  EXPECT_TRUE(ends_with(str, needle));
+}
+
+TEST(EndsWithTest, WorksWithCString)
+{
+  char *str = "foo";
+  char *needle = "o";
+
+  EXPECT_TRUE(ends_with(str, needle));
+}
+
+INSTANTIATE_TEST_CASE_P(Utils, EndsWithTest,
+    testing::Values(std::make_tuple("", "", true), std::make_tuple("foo", "", true),
+        std::make_tuple("foo", "o", true), std::make_tuple("foo", "oo", true),
+        std::make_tuple("foo", "foo", true), std::make_tuple("foo", "longer", false),
+        std::make_tuple("foo", "bar", false), std::make_tuple("foo bar", "foo", false)));
+
+}  // namespace utils::string
