8ec7f640f65b5c363205e4ea8588457d1626a841
Victor Barbu <victorbarbu08@gmail.com>
2019-10-18 00:14:54 +0300

    Reformat code in rfc7230_test

--- lib/microhttp/tests/rfc7230_test.cpp
+++ lib/microhttp/tests/rfc7230_test.cpp
@@ -39,7 +39,6 @@ TEST_P(StartLineTest, Works)
   }
 }
 
-
 using HeaderLineFixture = std::tuple<const char *, std::optional<HeaderLineData>>;
 
 class HeaderLineTest : public RFC7230, public testing::TestWithParam<HeaderLineFixture>
@@ -65,36 +64,36 @@ TEST_P(HeaderLineTest, Works)
   }
 }
 
+INSTANTIATE_TEST_CASE_P(RFC7230, StartLineTest,
+    testing::ValuesIn(std::vector<StartLineFixture>{
+        std::make_tuple("GET / HTTP/1.1\r\n", StartLineData{"GET", "/", Version{1, 1}}),  // valid
+        std::make_tuple("GET / HTTP/1.1", std::nullopt),  // no CRLF
+        std::make_tuple("GET / HTTP/v1.1\r\n", std::nullopt),  // invalid HTTP version
+        std::make_tuple("GET / \r\n", std::nullopt),  // no HTTP verwsion
+        std::make_tuple("GET HTTP/1.1\r\n", std::nullopt),  // no request target
+        std::make_tuple("/ HTTP/1.1\r\n", std::nullopt),  // no method
+        std::make_tuple("HTTP/1.1\r\n", std::nullopt),  // missing method and request target
+        std::make_tuple("", std::nullopt),  // obvious?
+        std::make_tuple(" GET / HTTP/1.1\r\n", std::nullopt),  // leading whitespace
+        std::make_tuple("GET / HTTP/1.1 \r\n", std::nullopt)  // trailing whitespace
+    }));
 
-INSTANTIATE_TEST_CASE_P(RFC7230, StartLineTest, testing::ValuesIn(std::vector<StartLineFixture>{
-  std::make_tuple("GET / HTTP/1.1\r\n", StartLineData{"GET", "/", Version{1, 1}}),  // valid
-  std::make_tuple("GET / HTTP/1.1", std::nullopt),  // no CRLF
-  std::make_tuple("GET / HTTP/v1.1\r\n", std::nullopt),  // invalid HTTP version
-  std::make_tuple("GET / \r\n", std::nullopt),  // no HTTP verwsion
-  std::make_tuple("GET HTTP/1.1\r\n", std::nullopt),  // no request target
-  std::make_tuple("/ HTTP/1.1\r\n", std::nullopt),  // no method
-  std::make_tuple("HTTP/1.1\r\n", std::nullopt),  // missing method and request target
-  std::make_tuple("", std::nullopt),  // obvious?
-  std::make_tuple(" GET / HTTP/1.1\r\n", std::nullopt),  // leading whitespace
-  std::make_tuple("GET / HTTP/1.1 \r\n", std::nullopt)  // trailing whitespace
-}));
-
-INSTANTIATE_TEST_CASE_P(RFC7230, HeaderLineTest, testing::ValuesIn(std::vector<HeaderLineFixture>{
-  std::make_tuple("name: value\r\n", HeaderLineData{"name", "value"}),  // valid
-  std::make_tuple("Header-Name: some value\r\n", HeaderLineData{"Header-Name", "some value"}),
-  std::make_tuple("Name:     value   \r\n", HeaderLineData{"Name", "value"}),
-  std::make_tuple("name: \r\n", HeaderLineData{"name", ""}),  // empty value
-  std::make_tuple("name:\r\n", HeaderLineData{"name", ""}),  // also empty value
-  std::make_tuple("Name: value", std::nullopt),  // no CRLF
-  std::make_tuple("Name:value\r\n", std::nullopt),  // no space after semicolon
-  std::make_tuple(" name: value\r\n", std::nullopt),  // leading space; unallowed
-  std::make_tuple("name : value\r\n", std::nullopt),  // space before semicolon
-  std::make_tuple("name. value\r\n", std::nullopt),  // no semicolon, so no value
-  std::make_tuple(": value\r\n", std::nullopt),  // no header name before semicolon
-  std::make_tuple(":\r\n", std::nullopt),  // empty header name and value
-  std::make_tuple("", std::nullopt),  // empty string
-  std::make_tuple("\r\n", std::nullopt),  // empty line
-}));
-
+INSTANTIATE_TEST_CASE_P(RFC7230, HeaderLineTest,
+    testing::ValuesIn(std::vector<HeaderLineFixture>{
+        std::make_tuple("name: value\r\n", HeaderLineData{"name", "value"}),  // valid
+        std::make_tuple("Header-Name: some value\r\n", HeaderLineData{"Header-Name", "some value"}),
+        std::make_tuple("Name:     value   \r\n", HeaderLineData{"Name", "value"}),
+        std::make_tuple("name: \r\n", HeaderLineData{"name", ""}),  // empty value
+        std::make_tuple("name:\r\n", HeaderLineData{"name", ""}),  // also empty value
+        std::make_tuple("Name: value", std::nullopt),  // no CRLF
+        std::make_tuple("Name:value\r\n", std::nullopt),  // no space after semicolon
+        std::make_tuple(" name: value\r\n", std::nullopt),  // leading space; unallowed
+        std::make_tuple("name : value\r\n", std::nullopt),  // space before semicolon
+        std::make_tuple("name. value\r\n", std::nullopt),  // no semicolon, so no value
+        std::make_tuple(": value\r\n", std::nullopt),  // no header name before semicolon
+        std::make_tuple(":\r\n", std::nullopt),  // empty header name and value
+        std::make_tuple("", std::nullopt),  // empty string
+        std::make_tuple("\r\n", std::nullopt),  // empty line
+    }));
 
 }  // namespace microhttp::http
